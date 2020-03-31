28fbe6e23ce4bff35e5f7763fd56e0fc052e98b9
sudhamurugan06 <50529864+sudhamurugan06@users.noreply.github.com>
2020-03-31 19:47:18 +0530

    Update Sort-string-based-on-consonants.c

--- Sort-string-based-on-consonants.c
+++ Sort-string-based-on-consonants.c
@@ -27,3 +27,41 @@ for(int i=0;i<50;i++){
 }
 }
 }
+************************************************************or***********************************************************
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+int main()
+{
+    int n,i,cnt,j,a[100]={0},min,pos;
+    char s[102][102];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%s",s[i]);
+    for(i=0;i<n;i++)
+    {
+        cnt=0;
+        for(j=0;j<strlen(s[i]);j++)
+        {
+            if(s[i][j]!='a' && s[i][j]!='e' && s[i][j]!='i' && s[i][j]!='o' && s[i][j]!='u' && s[i][j]!='A' && s[i][j]!='O' && s[i][j]!='E' && s[i][j]!='I' && s[i][j]!='U' )
+            cnt++;
+        }
+        a[i]=cnt;
+    }
+    for(i=0;i<n;i++)
+    {
+        min=103;
+        pos=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[j]!=-1 && min>a[j] )
+            {
+                min=a[j];
+                pos=j;
+            }
+        }
+        a[pos]=-1;
+        printf("%s ",s[pos]);
+    }
+    
+
